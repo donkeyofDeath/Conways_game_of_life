7d3aa6a72ffa2294c3be858054b58a49859eadd8
Tobias Liebmann <tobias.liebmann@gmx.de>
2021-04-19 19:26:12 +0200

    Added method for random initialization of the game grid.

--- game_of_life/game_of_life.h
+++ game_of_life/game_of_life.h
@@ -5,9 +5,10 @@
 #ifndef MAIN_CPP_GAME_OF_LIFE_H
 #define MAIN_CPP_GAME_OF_LIFE_H
 
-#include <cstdlib>
-#include <ctime>
+
 #include <array>
+#include <random>
+
 
 template<size_t dimension_x, size_t dimension_y>
 class GameOfLife {
@@ -24,12 +25,17 @@ class GameOfLife {
 
     void initialize_random_grid(){
 
-        
+        std::random_device rd;
+        std::mt19937 mt(rd());
+        std::uniform_int_distribution<int> dist(0, 1);
 
         for(int i = 0; i < dimension_x; i++){
-            for(int j = 0; j < dimension_j; j++){
+            for(int j = 0; j < dimension_y; j++){
 
-                grid[i][j] =
+                bool random_bool = dist(mt);
+
+                grid[i][j] = random_bool;
+
             }
         }
     }
