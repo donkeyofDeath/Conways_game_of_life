49e40a21ccb300ee70c8eab6c8fafcd4b6f96d95
Tobias Liebmann <tobias.liebmann@gmx.de>
2021-04-19 17:43:57 +0200

    Started working on method to initialize a random grid.

--- game_of_life/game_of_life.h
+++ game_of_life/game_of_life.h
@@ -5,8 +5,34 @@
 #ifndef MAIN_CPP_GAME_OF_LIFE_H
 #define MAIN_CPP_GAME_OF_LIFE_H
 
+#include <cstdlib>
+#include <ctime>
+#include <array>
 
+template<size_t dimension_x, size_t dimension_y>
 class GameOfLife {
+
+    int number_of_time_steps{};
+    std::array<std::array<bool, dimension_x>, dimension_y> grid{};
+
+
+    explicit GameOfLife(int number_of_time_steps){
+        this->number_of_time_steps = number_of_time_steps;
+        grid.fill({});
+    }
+
+
+    void initialize_random_grid(){
+
+        
+
+        for(int i = 0; i < dimension_x; i++){
+            for(int j = 0; j < dimension_j; j++){
+
+                grid[i][j] =
+            }
+        }
+    }
 
 };
 
