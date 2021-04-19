e44f81d084b8e9ea5f872eca1915fac254bb82b1
Tobias Liebmann <tobias.liebmann@gmx.de>
2021-04-19 16:54:22 +0200

    Added new folder for the game of life class.

---
+++ game_of_life/game_of_life.cpp
@@ -0,0 +1,5 @@
+//
+// Created by tobias on 19.04.21.
+//
+
+#include "game_of_life.h"
---
+++ game_of_life/game_of_life.h
@@ -0,0 +1,14 @@
+//
+// Created by tobias on 19.04.21.
+//
+
+#ifndef MAIN_CPP_GAME_OF_LIFE_H
+#define MAIN_CPP_GAME_OF_LIFE_H
+
+
+class GameOfLife {
+
+};
+
+
+#endif //MAIN_CPP_GAME_OF_LIFE_H
