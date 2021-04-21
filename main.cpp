8d2999c123fa269872c105651365bbaf9d8dd840
Tobias Liebmann <tobias.liebmann@gmx.de>
2021-04-21 02:31:32 +0200

    I am trying to make an animation displaing the game of life.

--- main.cpp
+++ main.cpp
@@ -1,23 +1,24 @@
 #include "game_of_life//game_of_life.h"
 #include <iostream>
+#include <unistd.h> // for sleep()
 
 int main() {
 
-    GameOfLife<5, 5> my_game_of_life(10);
+    int number_of_time_steps = 100;
 
-    my_game_of_life.update();
-
-    std::cout << "\n";
-
-    std::cout << "Last grid:\n";
-
-    my_game_of_life.print_last_grid();
-
-    std::cout << "\n";
-
-    std::cout << "Current grid:\n";
+    GameOfLife<5, 5> my_game_of_life(number_of_time_steps);
 
     my_game_of_life.print_current_grid();
+
+    for(int i = 0; i < number_of_time_steps; i++){
+        my_game_of_life.update();
+
+        my_game_of_life.print_current_grid();
+        sleep(1);
+        for(int i = 0; i < 100; i++){
+            std::cout << "\b" << std::flush;
+        }
+    }
 
     return 0;
 }
